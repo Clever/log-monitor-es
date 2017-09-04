976e806d24ea6b10c0cddb9ee4f97ea92a83e8b7
Rafael Garcia <rafael.garcia@clever.com>
2017-09-04 00:39:09 +0000

    don't log ES delivery lag for instances that aren't running

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/signalfx/golib/datapoint"
 	"github.com/signalfx/golib/sfxclient"
 	"golang.org/x/net/context"
@@ -104,6 +109,49 @@ func sendToSignalFX(timestamps map[string]time.Time) error {
 	return sfxSink.AddDatapoints(context.TODO(), points)
 }
 
+type ec2IPChecker struct {
+	ec2api            ec2iface.EC2API
+	lastCheck         time.Time
+	privateIPsRunning map[string]struct{}
+}
+
+func (e *ec2IPChecker) updateCache() error {
+	if e.privateIPsRunning != nil && time.Now().Sub(e.lastCheck) < 1*time.Minute {
+		return nil
+	}
+
+	privateIPsRunning := map[string]struct{}{}
+	if err := e.ec2api.DescribeInstancesPages(&ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{{
+			Name:   aws.String("instance-state-name"),
+			Values: []*string{aws.String("running")},
+		}},
+	}, func(output *ec2.DescribeInstancesOutput, lastPage bool) bool {
+		for _, res := range output.Reservations {
+			for _, instance := range res.Instances {
+				if instance.PrivateIpAddress != nil {
+					privateIPsRunning[*instance.PrivateIpAddress] = struct{}{}
+				}
+			}
+		}
+		return true
+	}); err != nil {
+		return err
+	}
+
+	e.privateIPsRunning = privateIPsRunning
+	e.lastCheck = time.Now()
+	return nil
+}
+
+func (e *ec2IPChecker) IsRunning(ip string) (bool, error) {
+	if err := e.updateCache(); err != nil {
+		return false, err
+	}
+	_, ok := e.privateIPsRunning[ip]
+	return ok, nil
+}
+
 func main() {
 	// For AWS logs-* clusters, access is controlled by IP address so no signing is needed,
 	// but since AWS blocks some APIs, sniffing and healthchecks are disabled.
@@ -118,12 +166,30 @@ func main() {
 		log.Fatalf("Failed to create ES client: %s\n", err)
 	}
 
+	sess := session.New()
+	ec2api := ec2.New(sess)
+	ec2ip := &ec2IPChecker{ec2api: ec2api}
+
 	tick := time.Tick(15 * time.Second)
 	for {
 		timestamps, err := getLatestTimestamps(esClient)
 		if err != nil {
 			kvlog.ErrorD("timestamp", kv.M{"error": err.Error()})
 			continue
+		}
+
+		// remove data for instances that aren't running
+		for hostname := range timestamps {
+			if strings.HasPrefix(hostname, "ip-") {
+				// parse IP address out of ES hostnames of the form ip-10-0-0-1
+				ip := strings.Replace(strings.TrimPrefix(hostname, "ip-"), "-", ".", -1)
+				running, err := ec2ip.IsRunning(ip)
+				if err != nil {
+					kvlog.ErrorD("ec2-ip-check", kv.M{"error": err.Error()})
+				} else if !running {
+					delete(timestamps, hostname)
+				}
+			}
 		}
 
 		// Log the number of hosts reported
