a0512a80ee0862a08631f87286cd9880bf3c6aae
Xavi Ramirez <xavi.ramirez@clever.com>
2018-11-27 18:06:31 -0800

    Reduced log level

--- main.go
+++ main.go
@@ -193,15 +193,13 @@ func main() {
 		}
 
 		// Log the number of hosts reported
-		kvlog.InfoD("timestamp", kv.M{
-			"count": len(timestamps),
-		})
+		kvlog.DebugD("timestamp", kv.M{"count": len(timestamps)})
 
 		err = sendToSignalFX(timestamps)
 		if err != nil {
 			kvlog.ErrorD("send-to-signalfx", kv.M{"error": err.Error()})
 			continue
 		}
-		kvlog.Info("sent-to-signalfx")
+		kvlog.Trace("sent-to-signalfx")
 	}
 }
